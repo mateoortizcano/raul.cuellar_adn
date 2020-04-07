93585ca6065985d932828d87f0397ad885fc8247
raul.cuellar <raul.cuellar@ceiba.com.co>
2020-04-07 18:41:23 -0500

    Eliminar lista innecesario de Usuarios

--- alcance-backend/microservicio/consulta/consulta-infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
+++ alcance-backend/microservicio/consulta/consulta-infraestructura/src/test/resources/db/migration/DML/V1.1__data.sql
@@ -1,3 +1,2 @@
-insert into usuario(nombre,clave,fecha_creacion) values('test','1234',now());
 insert into epica(titulo, estimacion_comercial) values('test','1234');
 insert into presupuesto(valor, reserva_estabilizacion) values(10,1);
