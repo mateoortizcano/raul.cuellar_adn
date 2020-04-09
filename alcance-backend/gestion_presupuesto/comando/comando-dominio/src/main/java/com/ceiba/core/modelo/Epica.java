3c33783ac5934ff508bdf8f4b3665920b345f1a0
raul.cuellar <raul.cuellar@ceiba.com.co>
2020-04-08 20:57:31 -0500

    Código innecesario

--- alcance-backend/gestion_presupuesto/comando/comando-dominio/src/main/java/com/ceiba/core/modelo/Epica.java
+++ alcance-backend/gestion_presupuesto/comando/comando-dominio/src/main/java/com/ceiba/core/modelo/Epica.java
@@ -24,12 +24,4 @@ public class Epica {
     public Long getId() {
         return id;
     }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public Integer getEstimacionComercial() {
-        return estimacionComercial;
-    }
 }
